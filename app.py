71df7dd74fac6f7dd1baae23eb5b10cfab7b5d0a
WiBertt <131600710+WiBertt@users.noreply.github.com>
2025-09-28 15:06:26 -0500

    Init

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, send_file
+from gtts import gTTS
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', test = "as")
+
+if __name__ == '__main__':
+    app.run(debug=True)
