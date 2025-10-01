6d1312c443983f6f50f13e68c12230d642546606
WiBertt <131600710+WiBertt@users.noreply.github.com>
2025-10-01 11:19:01 -0500

    pc

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    print("henffjwoenfownfoo")
-    return render_template('index.html', test = "as")
+def home():
+    return render_template('index.html')
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    return render_template('account.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
