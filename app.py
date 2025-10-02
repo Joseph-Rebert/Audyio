018cda3b8b6395011cf344c4580a7681b0326d78
Joseph Rebert <131600710+Joseph-Rebert@users.noreply.github.com>
2025-10-02 08:14:47 -0500

    Mac

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, url_for
 from gtts import gTTS
 import os
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -8,9 +10,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/account', methods=['GET', 'POST'])
-def account():
-    return render_template('account.html')
+@app.route('/<user>', methods=['GET', 'POST'])
+def user(user):
+    return f"<h1> {user} </h1>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+            username = request.form['username']
+            return redirect(url_for('user', user=username))
+    else:
+        return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
