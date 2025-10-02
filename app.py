049e3c97a6538bd7f524f9caa915c6a404d55f77
Joseph Rebert <131600710+joseph-rebert@users.noreply.github.com>
2025-10-02 08:14:47 -0500

    Added basic functionality of tts

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
+from contextlib import closing
+from io import BytesIO
+
+import boto3
 from flask import Flask, render_template, request, send_file, url_for
-from gtts import gTTS
 import os
 
-from werkzeug.utils import redirect
-
 app = Flask(__name__)
+polly = boto3.client('polly')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
-@app.route('/<user>', methods=['GET', 'POST'])
-def user(user):
-    return f"<h1> {user} </h1>"
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
-            username = request.form['username']
-            return redirect(url_for('user', user=username))
+        user_input = request.form['input']
+        try:
+            response = polly.synthesize_speech(Text=user_input, OutputFormat='mp3', VoiceId='Joanna')
+        except:
+            print("ERROR")
+
+        if "AudioStream" in response:
+            with closing(response["AudioStream"]) as stream:
+                audio_data = BytesIO(stream.read())
+                audio_data.seek(0)
+                return send_file(audio_data, mimetype='audio/mp3', as_attachment=True, download_name='speech.mp3')
     else:
-        return render_template('login.html')
+        return render_template('index.html')
+
 
 
 if __name__ == '__main__':
