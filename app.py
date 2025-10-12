fbbe2a6a0bf760d8b94ee8238be2c205235cb5bd
WiBertt <131600710+WiBertt@users.noreply.github.com>
2025-10-12 09:56:12 -0500

    EB added

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ def update_users_word_count(email, words_used):
         return False
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
